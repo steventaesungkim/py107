21802095ead681d416549d9d54f1e98bb8d477e0
Steven Taesung Kim <steventaesungkim@gmail.com>
2018-09-25 11:35:55 -0400

    practicing

--- exercise_1.py
+++ exercise_1.py
@@ -7,20 +7,28 @@ phonebook_dict = {
 }
 
 #1 Print elizabeth's phone number.
-print(phonebook_dict["Elizabeth"])
+print("Elizabeth's number: ", phonebook_dict["Elizabeth"])
 print()
 
 #2 Add an entry to the dictionary: Kareem's number is 938-489-1234.
 phonebook_dict["Kareem"] = "938-489-1234"
 
-print(phonebook_dict, end="")
+print("Kareem and Kareem's number added: ", phonebook_dict)
 print()
 
 #3 Delete Alice's phone entry.
 del phonebook_dict["Alice"]
 
-print(phonebook_dict, end= "")
+print("Alice phone entry is deleted: ", phonebook_dict)
 print()
 
 # 4 Change Bob's phone number to '968-345-2345'.
+phonebook_dict["Bob"] = "968-345-2345"
+
+print("Bob's number has been changed: ", phonebook_dict)
+print()
+
+#5 Print all the Phone entries
+print("Print out all the entries: ", phonebook_dict)
+print()
 
