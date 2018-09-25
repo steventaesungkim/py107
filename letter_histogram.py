4fa6b394b5694571738a2b93ca8d8085836c11b5
Steven Taesung Kim <steventaesungkim@gmail.com>
2018-09-25 13:58:03 -0400

    4

--- letter_histogram.py
+++ letter_histogram.py
@@ -4,15 +4,25 @@ user = input("banana".upper())
 
 bank = []
 
-alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","s","t","v","w","x","y","z"]
+def letter_count(letter, count):
+    counter = 0
+    for letter in user:
+        counter += (letter == count)
+        bank.append(bank[letter : count])
+    return counter
 
-for count in user:
-    if user == alpha[count]:
-        bank.append(alpha[user])
-        counter = 0
-        while counter < bank[counter]:
-            word_count = bank[counter].value()
-        counter += 1
+print(bank)
+print(bank[letter : count])
+
+
+
+# for count in user:
+#     if user == alpha[count]:
+#         bank.append(alpha[user])
+#         counter = 0
+#         while counter < bank[counter]:
+#             word_count = bank[counter].value()
+#         counter += 1
     
 
-print(word_count)
+# print(word_count)
