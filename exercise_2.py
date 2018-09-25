d20d631bb0efd6d0b5725bd657f1298d5dca96d8
Steven Taesung Kim <steventaesungkim@gmail.com>
2018-09-25 11:42:16 -0400

    practicing

--- exercise_2.py
+++ exercise_2.py
@@ -1,1 +1,23 @@
-#hello
+#Exercise 2 - Nesting dictionaries
+
+ramit = {
+  'name': 'Ramit',
+  'email': 'ramit@gmail.com',
+  'interests': ['movies', 'tennis'],
+  'friends': [
+    {
+      'name': 'Jasmine',
+      'email': 'jasmine@yahoo.com',
+      'interests': ['photography', 'tennis']
+    },
+    {
+      'name': 'Jan',
+      'email': 'jan@hotmail.com',
+      'interests': ['movies', 'tv']
+    }
+  ]
+}
+
+#1 Write a python expression that gets the email address of Ramit.
+print("Email address for Ramit: ", ramit["email"])
+print()
