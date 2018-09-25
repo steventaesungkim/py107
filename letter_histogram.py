4c42a7a898c124b83eb02ad1f4ccc6a4f9cf84f5
Steven Taesung Kim <steventaesungkim@gmail.com>
2018-09-25 13:20:15 -0400

    counting a word

---
+++ letter_histogram.py
@@ -0,0 +1,18 @@
+#User inputs a word and the dictionary collect and tally up how many times each letter in the alphabet was used in the word.
+
+user = input("banana".upper())
+
+bank = []
+
+alpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","s","t","v","w","x","y","z"]
+
+for count in alpha[user]:
+    if int(user) == alpha[count]:
+        bank.append(alpha[user])
+        counter = 0
+        while counter < bank[counter]:
+            word_count = bank[counter].value()
+        counter += 1
+    
+
+print(word_count)
