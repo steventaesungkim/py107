5d9a0a0c13e06efefcd22d57e31c15410c37d4ad
Steven Taesung Kim <steventaesungkim@gmail.com>
2018-09-25 20:23:12 -0400

    DONE

---
+++ word_histogram.py
@@ -0,0 +1,18 @@
+#User inputs a sentence and the dictionary will collect and tally up how many times each word is used in the sentence.
+
+sen = input("Input a sentence and it will count the words in the sentence: ").upper()
+word = []
+dic = {}
+num = []
+
+
+for w in str.split(sen):
+    if w not in word:
+        word.append(w)
+for w in word:
+    num.append(sen.count(w))
+for w in range(len(word)):
+    dic[word[w]] = num[w]
+
+print(dic)
+print()
